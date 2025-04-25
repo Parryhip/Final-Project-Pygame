7a9eaf80409162a5f24b25d5d5515b20681ef320
maximusholdaway11 <max.holdaway@ucas-edu.net>
2025-04-25 10:51:59 -0600

    Updated classes and tic tac toe stuff

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,33 +1,7 @@
 #Tic Tac Toe function / game
 import pygame
 import random
-
-class Button: 
-    def __init__(self, x, y, width, height, text, color, hover_color): 
-        self.rect = pygame.Rect(x, y, width, height) 
-        self.text = text 
-        self.color = color 
-        self.hover_color = hover_color 
-        self.font = pygame.font.Font(None, 36) 
- 
-    def draw(self, surface): 
-        # Change color on hover 
-        mouse_pos = pygame.mouse.get_pos() 
-        if self.rect.collidepoint(mouse_pos): 
-            pygame.draw.rect(surface, self.hover_color, self.rect) 
-        else: 
-            pygame.draw.rect(surface, self.color, self.rect) 
-         
-        # Render text 
-        text_surface = self.font.render(self.text, True, "Black") 
-        text_rect = text_surface.get_rect(center=self.rect.center) 
-        surface.blit(text_surface, text_rect) 
- 
-    def is_clicked(self): 
-        mouse_click = pygame.mouse.get_pressed() 
-        if mouse_click[0] and self.rect.collidepoint(pygame.mouse.get_pos()): 
-            return True 
-        return False
+from classes import *
 
 pygame.init()
 screen = pygame.display.set_mode((2560, 1375))
