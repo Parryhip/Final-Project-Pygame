733ae8fddb89481efbc6fdc6a6aa5d5e11c3d538
maximusholdaway11 <max.holdaway@ucas-edu.net>
2025-05-12 10:42:02 -0600

    Fixed bug and titles

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ from pong import *
 from sign_in import *
 
 pygame.init()
+font = pygame.font.SysFont(None, 72)
+game_selection_title = "Game Selection"
+game_selection_title_surface = font.render(game_selection_title, True, "White")
+main_title = "High Score Tracker"
+main_title_surface = font.render(main_title, True, "White")
 screen = pygame.display.set_mode((2560, 1375))
 clock = pygame.time.Clock()
 start_button = Button(screen.get_width() / 2 - 200, 900, 300, 100, "Start", "Blue", "White")
@@ -30,6 +35,7 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
         if start_button_pressed == False:
+            screen.blit(main_title_surface, (screen.get_width() / 2 - 275, 500))
             start_button.draw(screen)
             exit_button.draw(screen)
             if start_button.is_clicked():
@@ -38,25 +44,27 @@ def main():
             if exit_button.is_clicked():
                 running = False
         else:
+            screen.blit(game_selection_title_surface, (screen.get_width() / 2 - 250, 500))
             back_button.draw(screen)
             if back_button.is_clicked():
                 start_button_pressed = False
                 time.sleep(0.2)
-            tic_tac_button.draw(screen)
-            if tic_tac_button.is_clicked():
-                tic_tac_toe(username)
             click_per_minute_button.draw(screen)
             if click_per_minute_button.is_clicked():
-                cpm(username)
+                cpm(username, screen, clock)
             pong_button.draw(screen)
             if pong_button.is_clicked():
-                pong_loop(username)
+                pong_loop(username, screen, clock)
             platformer_button.draw(screen)
             if platformer_button.is_clicked():
                 pass
             reaction_speed_button.draw(screen)
             if reaction_speed_button.is_clicked():
                 pass
+            tic_tac_button.draw(screen)
+            if tic_tac_button.is_clicked():
+                tic_tac_toe(username, screen, clock)
+                screen.fill("black")
         pygame.display.flip()
         clock.tick(60)
     pygame.quit()
